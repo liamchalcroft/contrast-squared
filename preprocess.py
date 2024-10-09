4d9fd183ad642304c0c9001eef0e44e2b85f7251
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-09 18:11:31 +0100

    ++

--- preprocess.py
+++ preprocess.py
@@ -93,7 +93,7 @@ def get_bloch_loader(
                 upper=99.5,
             ),  # just clip extreme values, don't rescale
             mn.transforms.LambdaD(keys=["image1"], func=mn.transforms.SignalFillEmpty()),
-            mn.transforms.SpacingD(keys=["image1"], pixdim=2 if lowres else 1),
+            # mn.transforms.SpacingD(keys=["image1"], pixdim=2 if lowres else 1),
             mn.transforms.ToTensorD(dtype=torch.float32, keys=["image1"], device=device)]
     
     if same_contrast:
