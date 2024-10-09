7fa15c986bbd1373e6d87720d67528d844628d36
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-09 20:51:59 +0100

    ++

--- bloch.py
+++ bloch.py
@@ -342,7 +342,9 @@ class MONAIBlochTransform(Transform):
 class MONAIBlochTransformD(MapTransform):
     def __init__(self, keys, num_ch=1, **kwargs):
         super().__init__(keys)
-        self.bloch = MONAIBlochTransform(num_ch=num_ch, **kwargs)
+        self.transform = MONAIBlochTransform(num_ch=num_ch, **kwargs)
 
     def __call__(self, data):
-        return self.bloch(data[self.keys[0]])
+        for key in self.keys:
+            data[key] = self.transform(data[key])
+        return data
