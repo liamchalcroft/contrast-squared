cc225058535934ece745b97426e82ef9a8769116
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-20 12:15:36 +0100

    ++

--- brainage_train.py
+++ brainage_train.py
@@ -96,6 +96,7 @@ def run_model(args):
         model.train()
         train_loss = 0.0
         for features, ages in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+            print(features)
             print(ages)
             optimizer.zero_grad()
             outputs = model(features)
