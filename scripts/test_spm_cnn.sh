7189ed136749534dd17621123fc13ff0b6d31403
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-27 18:09:47 +0000

    Add script for SPM testing

---
+++ scripts/test_spm_cnn.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# 100% of training data
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-mprage-pc100/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-pc100/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+# 10% of training data
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-mprage-pc10/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-pc10/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+# 1% of training data
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-mprage-pc1/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-pc1/checkpoint.pt \
+    --net cnn \
+    --amp \
+
+python ../test_seg_healthy.py \
+    --weights ../spm-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
+    --net cnn \
+    --amp \
