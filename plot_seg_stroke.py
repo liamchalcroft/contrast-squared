6b9c484242e1c7ef2a47ac1271fcad2ab28ffd43
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-28 00:58:38 +0000

    ++

--- plot_seg_stroke.py
+++ plot_seg_stroke.py
@@ -10,6 +10,7 @@ def get_results_df(results_dir):
     if os.path.exists(results_file):
         df = pd.read_csv(results_file)
         run_name = results_dir.split("/")[-1]
+        print(f"run_name: {run_name}")
         data_pc = int(run_name.split("pc")[1])
         df["% Training Data"] = data_pc
         method = run_name.split("simclr-")[1].split("-")[0]
