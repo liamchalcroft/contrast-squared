47962673212b2e8c7a3cf7993be246cfd2b4d1ff
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-10-31 11:11:08 +0000

    ++

--- scripts/stroke_cnn_pc100.sh
+++ scripts/stroke_cnn_pc100.sh
@@ -36,71 +36,71 @@ python ../train_seg_stroke.py \
     --resume
 
 
-# BLOCH
-python ../train_seg_stroke.py \
-    --name stroke-t1-cnn-simclr-bloch-pc100 \
-    --net cnn \
-    --modality t1 \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# # BLOCH
+# python ../train_seg_stroke.py \
+#     --name stroke-t1-cnn-simclr-bloch-pc100 \
+#     --net cnn \
+#     --modality t1 \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
 
-python ../train_seg_stroke.py \
-    --name stroke-t2-cnn-simclr-bloch-pc100 \
-    --net cnn \
-    --modality t2 \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# python ../train_seg_stroke.py \
+#     --name stroke-t2-cnn-simclr-bloch-pc100 \
+#     --net cnn \
+#     --modality t2 \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
 
-python ../train_seg_stroke.py \
-    --name stroke-flair-cnn-simclr-bloch-pc100 \
-    --net cnn \
-    --modality flair \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# python ../train_seg_stroke.py \
+#     --name stroke-flair-cnn-simclr-bloch-pc100 \
+#     --net cnn \
+#     --modality flair \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
 
 
-# BLOCH PAIRED
-python ../train_seg_stroke.py \
-    --name stroke-t1-cnn-simclr-bloch-paired-pc100 \
-    --net cnn \
-    --modality t1 \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# # BLOCH PAIRED
+# python ../train_seg_stroke.py \
+#     --name stroke-t1-cnn-simclr-bloch-paired-pc100 \
+#     --net cnn \
+#     --modality t1 \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
 
-python ../train_seg_stroke.py \
-    --name stroke-t2-cnn-simclr-bloch-paired-pc100 \
-    --net cnn \
-    --modality t2 \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# python ../train_seg_stroke.py \
+#     --name stroke-t2-cnn-simclr-bloch-paired-pc100 \
+#     --net cnn \
+#     --modality t2 \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
 
-python ../train_seg_stroke.py \
-    --name stroke-flair-cnn-simclr-bloch-paired-pc100 \
-    --net cnn \
-    --modality flair \
-    --amp \
-    --logdir ../ \
-    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-    --pc_data 100 \
-    --debug \
-    --resume
+# python ../train_seg_stroke.py \
+#     --name stroke-flair-cnn-simclr-bloch-paired-pc100 \
+#     --net cnn \
+#     --modality flair \
+#     --amp \
+#     --logdir ../ \
+#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+#     --pc_data 100 \
+#     --debug \
+#     --resume
