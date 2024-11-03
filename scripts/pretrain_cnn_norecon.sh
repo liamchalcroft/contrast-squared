c4930fdc3881eb6de1aa8dcffa062452ff80e58c
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-03 15:18:26 +0000

    ++

---
+++ scripts/pretrain_cnn_norecon.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+python ../pretrain_3d.py \
+    --name 3d-cnn-simclr-mprage-norecon \
+    --batch_size 8 \
+    --epochs 300 \
+    --epoch_length 200 \
+    --lr 1e-3 \
+    --loss simclr \
+    --data mprage \
+    --no_recon \
+    --net cnn \
+    --amp \
+    --logdir ../ \
+    --debug \
+    --resume
+
+python ../pretrain_3d.py \
+    --name 3d-cnn-simclr-bloch-norecon \
+    --batch_size 8 \
+    --epochs 300 \
+    --epoch_length 200 \
+    --lr 1e-3 \
+    --loss simclr \
+    --data bloch \
+    --no_recon \
+    --net cnn \
+    --amp \
+    --logdir ../ \
+    --debug \
+    --resume
+
+python ../pretrain_3d.py \
+    --name 3d-cnn-simclr-bloch-paired-norecon \
+    --batch_size 8 \
+    --epochs 300 \
+    --epoch_length 200 \
+    --lr 1e-3 \
+    --loss simclr \
+    --data bloch-paired \
+    --no_recon \
+    --net cnn \
+    --amp \
+    --logdir ../ \
+    --debug \
+    --resume
