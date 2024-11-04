6d9f26358e9197fe12f348a0adbba9d68d22745c
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-04 00:29:09 +0000

    ++

--- scripts/spm_cnn_pc100.sh
+++ scripts/spm_cnn_pc100.sh
@@ -36,71 +36,71 @@ python ../train_seg_healthy.py \
     --resume
 
 
-# # BLOCH
-# python ../train_seg_healthy.py \
-#     --name spm-t1-cnn-simclr-bloch-pc100 \
-#     --net cnn \
-#     --modality t1 \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+# BLOCH
+python ../train_seg_healthy.py \
+    --name spm-t1-cnn-simclr-bloch-pc100 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
 
-# python ../train_seg_healthy.py \
-#     --name spm-t2-cnn-simclr-bloch-pc100 \
-#     --net cnn \
-#     --modality t2 \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+python ../train_seg_healthy.py \
+    --name spm-t2-cnn-simclr-bloch-pc100 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
 
-# python ../train_seg_healthy.py \
-#     --name spm-pd-cnn-simclr-bloch-pc100 \
-#     --net cnn \
-#     --modality pd \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+python ../train_seg_healthy.py \
+    --name spm-pd-cnn-simclr-bloch-pc100 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
 
 
-# # BLOCH PAIRED
-# python ../train_seg_healthy.py \
-#     --name spm-t1-cnn-simclr-bloch-paired-pc100 \
-#     --net cnn \
-#     --modality t1 \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+# BLOCH PAIRED
+python ../train_seg_healthy.py \
+    --name spm-t1-cnn-simclr-bloch-paired-pc100 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
 
-# python ../train_seg_healthy.py \
-#     --name spm-t2-cnn-simclr-bloch-paired-pc100 \
-#     --net cnn \
-#     --modality t2 \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+python ../train_seg_healthy.py \
+    --name spm-t2-cnn-simclr-bloch-paired-pc100 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
 
-# python ../train_seg_healthy.py \
-#     --name spm-pd-cnn-simclr-bloch-paired-pc100 \
-#     --net cnn \
-#     --modality pd \
-#     --amp \
-#     --logdir ../ \
-#     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
-#     --pc_data 100 \
-#     --debug \
-#     --resume
+python ../train_seg_healthy.py \
+    --name spm-pd-cnn-simclr-bloch-paired-pc100 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 100 \
+    --debug \
+    --resume
