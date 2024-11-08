861f721a577167168bfd4860e158c4f6bd57ebec
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-08 11:00:44 +0000

    Update age scripts

--- scripts/age_cnn_pc1.sh
+++ scripts/age_cnn_pc1.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# MPRAGE
 python ../train_classifier.py \
-    --name age-cnn-simclr-mprage-pc1 \
+    --name age-t1-cnn-simclr-mprage-pc1 \
     --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
@@ -11,8 +13,32 @@ python ../train_classifier.py \
     --resume
 
 python ../train_classifier.py \
-    --name age-cnn-simclr-bloch-pc1 \
+    --name age-t2-cnn-simclr-mprage-pc1 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_classifier.py \
+    --name age-pd-cnn-simclr-mprage-pc1 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+# BLOCH
+python ../train_classifier.py \
+    --name age-t1-cnn-simclr-bloch-pc1 \
+    --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
@@ -21,8 +47,54 @@ python ../train_classifier.py \
     --resume
 
 python ../train_classifier.py \
-    --name age-cnn-simclr-bloch-paired-pc1 \
+    --name age-t2-cnn-simclr-bloch-pc1 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_classifier.py \
+    --name age-pd-cnn-simclr-bloch-pc1 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+# BLOCH PAIRED
+python ../train_classifier.py \
+    --name age-t1-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_classifier.py \
+    --name age-t2-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_classifier.py \
+    --name age-pd-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality pd \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
