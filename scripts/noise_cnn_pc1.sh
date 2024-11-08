4db93b87ce33ecaf1bfdbe40aff06d37710d4b74
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-08 21:23:51 +0000

    Update noise scripts

--- scripts/noise_cnn_pc1.sh
+++ scripts/noise_cnn_pc1.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# MPRAGE
 python ../train_denoise.py \
-    --name noise-cnn-simclr-mprage-pc1 \
+    --name noise-t1-cnn-simclr-mprage-pc1 \
     --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
@@ -11,8 +13,32 @@ python ../train_denoise.py \
     --resume
 
 python ../train_denoise.py \
-    --name noise-cnn-simclr-bloch-pc1 \
+    --name noise-t2-cnn-simclr-mprage-pc1 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-mprage-pc1 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+# BLOCH
+python ../train_denoise.py \
+    --name noise-t1-cnn-simclr-bloch-pc1 \
+    --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
@@ -21,8 +47,54 @@ python ../train_denoise.py \
     --resume
 
 python ../train_denoise.py \
-    --name noise-cnn-simclr-bloch-paired-pc1 \
+    --name noise-t2-cnn-simclr-bloch-pc1 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-bloch-pc1 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+# BLOCH PAIRED
+python ../train_denoise.py \
+    --name noise-t1-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-t2-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 1 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-bloch-paired-pc1 \
+    --net cnn \
+    --modality pd \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
--- scripts/noise_cnn_pc10.sh
+++ scripts/noise_cnn_pc10.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# MPRAGE
 python ../train_denoise.py \
-    --name noise-cnn-simclr-mprage-pc10 \
+    --name noise-t1-cnn-simclr-mprage-pc10 \
     --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
@@ -11,8 +13,32 @@ python ../train_denoise.py \
     --resume
 
 python ../train_denoise.py \
-    --name noise-cnn-simclr-bloch-pc10 \
+    --name noise-t2-cnn-simclr-mprage-pc10 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-mprage-pc10 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+# BLOCH
+python ../train_denoise.py \
+    --name noise-t1-cnn-simclr-bloch-pc10 \
+    --net cnn \
+    --modality t1 \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
@@ -21,8 +47,54 @@ python ../train_denoise.py \
     --resume
 
 python ../train_denoise.py \
-    --name noise-cnn-simclr-bloch-paired-pc10 \
+    --name noise-t2-cnn-simclr-bloch-pc10 \
     --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-bloch-pc10 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+# BLOCH PAIRED
+python ../train_denoise.py \
+    --name noise-t1-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-t2-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_denoise.py \
+    --name noise-pd-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality pd \
     --amp \
     --logdir ../ \
     --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
