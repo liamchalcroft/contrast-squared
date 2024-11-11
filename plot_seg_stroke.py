3ae740716b1c48cde85222ca48fdbbe58c84afd9
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-11 16:17:05 +0000

    ++

--- plot_seg_stroke.py
+++ plot_seg_stroke.py
@@ -141,7 +141,10 @@ for percentage in training_percentages:
     plt.figure(figsize=(15, 8))
     ax = sns.violinplot(data=percentage_df, x="Modality Dataset", y="DSC", hue="Method",
                         hue_order=[MODEL_NAMES[m] for m in MODEL_ORDER],
-                        density_norm='width', inner='quartile', alpha=0.5)
+                        density_norm='width', inner='quartile', alpha=0.5,
+                        # span=[0, 100], spanmode='manual'
+                        spanmode='range'
+                        )
     plt.title(f"Dice Similarity Coefficient by Modality and Dataset ({percentage}% Training Data)", 
              pad=20, fontsize=16, fontweight='bold')
     plt.xlabel("Modality Dataset", fontsize=14, labelpad=15)
