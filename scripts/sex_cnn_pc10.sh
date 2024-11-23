8e52f6d909b561e713d7e13319343a1f6cb6909b
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-11-23 16:49:41 +0000

    ++

---
+++ scripts/sex_cnn_pc10.sh
@@ -0,0 +1,103 @@
+#! /bin/bash
+
+# MPRAGE
+python ../train_classifier_sex.py \
+    --name sex-t1-cnn-simclr-mprage-pc10 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-t2-cnn-simclr-mprage-pc10 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-pd-cnn-simclr-mprage-pc10 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-mprage/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+# BLOCH
+python ../train_classifier_sex.py \
+    --name sex-t1-cnn-simclr-bloch-pc10 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-t2-cnn-simclr-bloch-pc10 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-pd-cnn-simclr-bloch-pc10 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+# BLOCH PAIRED
+python ../train_classifier_sex.py \
+    --name sex-t1-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality t1 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-t2-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality t2 \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
+
+python ../train_classifier_sex.py \
+    --name sex-pd-cnn-simclr-bloch-paired-pc10 \
+    --net cnn \
+    --modality pd \
+    --amp \
+    --logdir ../ \
+    --backbone_weights ../3d-cnn-simclr-bloch-paired/checkpoint.pt \
+    --pc_data 10 \
+    --debug \
+    --resume
