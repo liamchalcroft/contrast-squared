39e7a9c21f2024653a05c32ba13918440b4f3c9c
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-12-03 10:24:54 +0000

    Use best metric checkpoint for stroke pred

--- scripts/test_stroke_cnn.sh
+++ scripts/test_stroke_cnn.sh
@@ -3,49 +3,49 @@
 ## 100% of training data
 # t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-mprage-pc100/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-mprage-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-pc100/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-mprage-pc100/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-mprage-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-pc100/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 # flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-mprage-pc100/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-mprage-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-pc100/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc100/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
@@ -54,49 +54,49 @@ python ../test_seg_stroke.py \
 ## 10% of training data
 # t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-mprage-pc10/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-mprage-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-pc10/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-mprage-pc10/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-mprage-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-pc10/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 # flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-mprage-pc10/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-mprage-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-pc10/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc10/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
@@ -105,49 +105,49 @@ python ../test_seg_stroke.py \
 ## 1% of training data
 # t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-mprage-pc1/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-mprage-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-pc1/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 python ../test_seg_stroke.py \
-    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
+    --weights ../stroke-t1-cnn-simclr-bloch-paired-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-mprage-pc1/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-mprage-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-pc1/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 python ../test_seg_stroke.py \
-    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
+    --weights ../stroke-t2-cnn-simclr-bloch-paired-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality t2
 # flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-mprage-pc1/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-mprage-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-pc1/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
 python ../test_seg_stroke.py \
-    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
+    --weights ../stroke-flair-cnn-simclr-bloch-paired-pc1/checkpoint_best.pt \
     --net cnn \
     --amp \
     --modality flair
