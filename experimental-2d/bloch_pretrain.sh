a516d66aa875ab8c585326a67d9acca523bcba75
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-12-15 13:15:18 +0000

    Remove multi-view vit stuff for now

--- experimental-2d/bloch_pretrain.sh
+++ experimental-2d/bloch_pretrain.sh
@@ -11,20 +11,20 @@ python pretrain.py --dataset bloch --model_name timm/resnet50.a1_in1k --pretrain
 # CLIP ViT-B/16
 # 2 views
 python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view2 --wandb_name bloch-vitclip-view2
-# 3 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view3 --wandb_name bloch-vitclip-view3
-# 4 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view4 --wandb_name bloch-vitclip-view4
-# 5 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view5 --wandb_name bloch-vitclip-view5
+# # 3 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view3 --wandb_name bloch-vitclip-view3
+# # 4 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view4 --wandb_name bloch-vitclip-view4
+# # 5 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitclip-view5 --wandb_name bloch-vitclip-view5
 
 # DINO ViT-B/16
 # 2 views
 python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view2 --wandb_name bloch-vitdino-view2
-# 3 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view3 --wandb_name bloch-vitdino-view3
-# 4 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view4 --wandb_name bloch-vitdino-view4
-# 5 views
-python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view5 --wandb_name bloch-vitdino-view5
+# # 3 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view3 --wandb_name bloch-vitdino-view3
+# # 4 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view4 --wandb_name bloch-vitdino-view4
+# # 5 views
+# python pretrain.py --dataset bloch --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-vitdino-view5 --wandb_name bloch-vitdino-view5
 
--- experimental-2d/bloch_same_pretrain.sh
+++ experimental-2d/bloch_same_pretrain.sh
@@ -11,20 +11,20 @@ python pretrain.py --dataset bloch --same_contrast --model_name timm/resnet50.a1
 # CLIP ViT-B/16
 # 2 views
 python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view2 --wandb_name bloch-same-vitclip-view2
-# 3 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view3 --wandb_name bloch-same-vitclip-view3
-# 4 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view4 --wandb_name bloch-same-vitclip-view4
-# 5 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view5 --wandb_name bloch-same-vitclip-view5
+# # 3 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view3 --wandb_name bloch-same-vitclip-view3
+# # 4 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view4 --wandb_name bloch-same-vitclip-view4
+# # 5 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_clip_224.openai --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitclip-view5 --wandb_name bloch-same-vitclip-view5
 
 # DINO ViT-B/16
 # 2 views
 python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view2 --wandb_name bloch-same-vitdino-view2
-# 3 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view3 --wandb_name bloch-same-vitdino-view3
-# 4 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view4 --wandb_name bloch-same-vitdino-view4
-# 5 views
-python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view5 --wandb_name bloch-same-vitdino-view5
+# # 3 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view3 --wandb_name bloch-same-vitdino-view3
+# # 4 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view4 --wandb_name bloch-same-vitdino-view4
+# # 5 views
+# python pretrain.py --dataset bloch --same_contrast --model_name timm/vit_base_patch16_224.dino --pretrained --wandb_entity atlas-ploras --checkpoint_dir checkpoints/bloch-same-vitdino-view5 --wandb_name bloch-same-vitdino-view5
 
