d21f85e7a97967c519c64e1cf72ed9f02cd6c1c2
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-12-18 16:59:53 +0000

    ++

--- experimental-2d/verify_ixi_dataset.py
+++ experimental-2d/verify_ixi_dataset.py
@@ -42,5 +42,9 @@ def verify_dataloaders(task='denoising', modalities=['t1', 't2', 'pd'], sites=['
     plt.close()
 
 if __name__ == "__main__":
-    for task in ['denoising', 'segmentation', 'classification']:
+    for task in [
+        # 'denoising', 
+        'segmentation', 
+        'classification'
+    ]:
         verify_dataloaders(task=task)
