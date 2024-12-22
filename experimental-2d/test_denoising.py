79611817acaf1ec480a4990c67ae71c332640916
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2024-12-22 00:30:26 +0000

    ++

--- experimental-2d/test_denoising.py
+++ experimental-2d/test_denoising.py
@@ -65,7 +65,7 @@ def test_denoising(model_dir, model_name, modality, site):
             clean = batch['image'].to(device)
             
             # Add noise
-            std = torch.rand(1, dtype=clean.dtype, device=clean.device) * 0.2
+            std = 0.05
             noise = torch.randn_like(clean) * std
             noisy = clean + noise
             
