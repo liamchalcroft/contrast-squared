598b07a6a2687a3048c7f4bb902b2bc7f660dbb1
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2025-01-17 11:06:57 +0000

    ++

--- scripts/test_sex_cnn.sh
+++ scripts/test_sex_cnn.sh
@@ -2,49 +2,49 @@
 
 ## 100% of training data
 # t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-mprage-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-mprage-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
 # pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-mprage-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-pc100/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-paired-pc100/checkpoint.pt \
     --net cnn \
     --amp \
@@ -52,49 +52,49 @@ python ../test_classifier_sex.py \
 
 ## 10% of training data
 # t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-mprage-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-mprage-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
 # pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-mprage-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-pc10/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-paired-pc10/checkpoint.pt \
     --net cnn \
     --amp \
@@ -102,49 +102,49 @@ python ../test_classifier_sex.py \
 
 ## 1% of training data
 # t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-mprage-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t1-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t1
 # t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-mprage-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-t2-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality t2
 # pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-mprage-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-pc1/checkpoint.pt \
     --net cnn \
     --amp \
     --modality pd
-python ../test_classifier_sex.py \
+python ../test_sex_classification.py \
     --weights ../sex-pd-cnn-simclr-bloch-paired-pc1/checkpoint.pt \
     --net cnn \
     --amp \
